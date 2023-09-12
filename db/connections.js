fa8b4469987bfe900146e1f80be840788d16cba2
CWells1225 <ebecochuba@yahoo.com>
2023-09-11 20:42:57 -0400

    established connection to mongodb

---
+++ db/connections.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const mongoConnect = async uri => { 
+    try {
+        await mongoose.connect(uri)
+    } catch (error){
+        console.log('error connecting to mongoDB')
+    }
+} 
