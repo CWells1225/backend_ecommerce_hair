7133b59eae3648e1243cb390f59c2960e45300b9
mnebot86 <miguelnbt@gmail.com>
2023-09-23 15:12:56 -0400

    Config Eslint
    - create lint config fold to enforce syntax style for clean code
    - created github workflow to text lint before merging to dev branch
    - created a pull request template for github to guide developer on what to mention in pr comments

--- db/connections.js
+++ db/connections.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const mongoConnect = async uri => { 
-    try {
-        await mongoose.connect(uri)
-    } catch (error){
-        console.log('error connecting to mongoDB')
-    }
-} 
+	try {
+		await mongoose.connect(uri);
+	} catch (error){
+		console.log('error connecting to mongoDB');
+	}
+}; 
