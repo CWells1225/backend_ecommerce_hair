3daae6cbfa065e6140a8779ede4f47895e805eb3
CWells1225 <ebecochuba@yahoo.com>
2023-09-30 17:54:16 -0400

    added commas

--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     fName: {
         type: String, 
-        required: [true, 'Please type in first name']
+        required: [true, 'Please type in first name'],
     }, 
     lName: {
         type: String, 
-        required: [true, 'Please type in last name']
+        required: [true, 'Please type in last name'],
     }, 
     email: {
         type: String, 
-        required: [true, 'Please type in email']
+        required: [true, 'Please type in email'],
     }, 
     password: {
         type: String, 
-        required: [true, 'Please type in password']
+        required: [true, 'Please type in password'],
     }
 }); 
 
