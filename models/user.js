df27e4ce2361680333a08b0487c78fa7deaebf06
CWells1225 <ebecochuba@yahoo.com>
2023-09-30 17:43:38 -0400

    user model schema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    fName: {
+        type: String, 
+        required: [true, 'Please type in first name']
+    }, 
+    lName: {
+        type: String, 
+        required: [true, 'Please type in last name']
+    }, 
+    email: {
+        type: String, 
+        required: [true, 'Please type in email']
+    }, 
+    password: {
+        type: String, 
+        required: [true, 'Please type in password']
+    }
+}); 
+
+module.exports = mongoose.model('User', userSchema);
