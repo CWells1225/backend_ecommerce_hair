8d64944729e06732ca2f4222ac0b079048704c38
CWells1225 <ebecochuba@yahoo.com>
2023-09-30 18:46:49 -0400

    updated use schema

--- models/user.js
+++ models/user.js
@@ -4,18 +4,21 @@ const userSchema = new mongoose.Schema({
     fName: {
         type: String, 
         required: [true, 'Please type in first name'],
+        trim: true
     }, 
     lName: {
         type: String, 
         required: [true, 'Please type in last name'],
+        trim: true
     }, 
     email: {
         type: String, 
         required: [true, 'Email is required.'],
+        unique: true
     }, 
     password: {
         type: String, 
-        required: [true, 'Password is required.'],
+        required: [true, 'Password is required.']
     }
 }); 
 
