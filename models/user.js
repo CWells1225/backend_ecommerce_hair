8f30a62b0d244ee5917da86bbd071445258d52f7
CWells1225 <ebecochuba@yahoo.com>
2023-10-15 14:13:01 -0400

    updates to user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     fName: {
@@ -25,7 +26,15 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String, 
         required: [true, 'Password is required.']
-    }
+    }, 
+    createdAt: {
+        type: Date, 
+        default: new Date(),
+    },
 }); 
+
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 12);
+})
 
 export default mongoose.model('User', userSchema);
